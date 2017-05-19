339c9bb4e4f697460748246bb181e0caccdabf9c
Ghabriel <ghabriel.nunes@gmail.com>
2017-05-19 13:01:44 -0300

    .tar.gz and .zip working

--- TestZone/extractor.sh
+++ TestZone/extractor.sh
@@ -8,16 +8,27 @@ filename="${filename%%.*}"
 if [ -d "$filename"  ]; then
     echo "Folder '$filename' already exists."
 else
-	mkdir $filename
-	cd $filename
-    echo "Filename: $filename"
-    echo "Extension: $extension"
-	case "$extension" in
-		"tar.gz")
-			tar -xvf $path
-		;;
-		"zip")
-			unzip $path
-	esac
-fi
+    mkdir $filename
+    cd $filename
+    case "$extension" in
+        "tar.gz")
+            tar -xvf ../$path
+        ;;
+        "zip")
+            unzip ../$path
+        ;;
+        *)
+            echo "Unsuported file type"
+        ;;
+    esac
 
+    count=0
+    for file in $(ls); do
+        count=$[$count+1]
+    done
+
+    if [[ $count == 1 && -d "$filename" ]]; then
+        mv $filename/* .
+        rmdir $filename
+    fi
+fi
