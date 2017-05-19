7e8342cf8e14ef21197046ef0bf7df03c0e47d7a
Ghabriel <ghabriel.nunes@gmail.com>
2017-05-19 13:51:32 -0300

    Began a refactor of the extractor

--- TestZone/extractor.sh
+++ TestZone/extractor.sh
@@ -2,33 +2,42 @@
 
 path=$1
 filename=$(basename "$path")
-extension="${filename#*.}"
-filename="${filename%%.*}"
 
-if [ -d "$filename"  ]; then
-    echo "Folder '$filename' already exists."
-else
-    mkdir $filename
-    cd $filename
-    case "$extension" in
-        "tar.gz")
-            tar -xvf ../$path
-        ;;
-        "zip")
-            unzip ../$path
-        ;;
-        *)
-            echo "Unsuported file type"
-        ;;
-    esac
-
-    count=0
-    for file in $(ls); do
-        count=$[$count+1]
-    done
-
-    if [[ $count == 1 && -d "$filename" ]]; then
-        mv $filename/* .
-        rmdir $filename
+function check() {
+    extension=$1
+    length=${#extension}
+    end=${filename:(-$length):$length}
+    if [[ "$extension" == "$end" ]]; then
+        filename=${filename:0:(-$length)}
+        if [[ -d "$filename" ]]; then
+            echo >&2 "Folder '$filename' already exists."
+            exit 1
+        else
+            mkdir $filename
+            cd $filename
+        fi
+        ret=1
+    else
+        ret=0
     fi
+    echo "$ret"
+}
+
+if [[ $(check ".tar.gz") == "1" ]]; then
+    tar -xvf ../$path
+elif [[ $(check ".zip") == "1" ]]; then
+    unzip ../$path
+else
+    echo "Unknown file type"
+    exit 1
+fi
+
+count=0
+for file in $(ls); do
+    count=$[$count+1]
+done
+
+if [[ $count == 1 && -d "$filename" ]]; then
+    mv $filename/* .
+    rmdir $filename
 fi
